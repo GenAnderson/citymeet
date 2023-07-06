d523a9b3835b94ba1ad73f27b258350819002558
GenAnderson <genchung1@outlook.com>
2023-07-06 13:41:13 -1000

    troubleshooting 4.3

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -6,7 +6,7 @@ const SCOPES = [
   "https://www.googleapis.com/auth/calendar.events.public.readonly",
 ];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
-const redirect_uris = ["https%3A%2F%2Fgenanderson.github.io%2Fcitymeet%2F"];
+const redirect_uris = ["https://genanderson.github.io/citymeet/"];
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
